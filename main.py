0e6f3f027bbe2a6ab7e4c760a649ca0b328b6313
JosueCarlosCamilo <camilo12lopezro@gmail.com>
2025-07-22 11:23:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+import constants
+
+#Inicializar pygame
+pygame.init()
+
+ventana = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
+pygame.display.set_caption("Simulador de vida salvaje")
+
