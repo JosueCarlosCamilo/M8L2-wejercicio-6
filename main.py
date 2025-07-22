ee2858d4aaeb887b0e9f94b866a3db3b3927d298
Camilo <camilo12lopezro@gmail.com>
2025-07-22 17:38:45 -0500

    CambiodeRama

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import constants
 #Inicializar pygame
 pygame.init()
 
+WIDTH, HEIGHT = 800, 600
 ventana = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
 pygame.display.set_caption("Simulador de vida salvaje")
-
+ 
