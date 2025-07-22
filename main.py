8bed22e710fcb6993c798b830cfbafe253376250
JosueCarlosCamilo <camilo12lopezro@gmail.com>
2025-07-22 18:34:34 -0500

    main.py

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ WIDTH, HEIGHT = 800, 600
 ventana = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
 pygame.display.set_caption("Simulador de vida salvaje")
 print("Nueva rama")
+print("Tarea 6")
