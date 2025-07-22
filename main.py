dcf460344eac601cbea2523b9d504b79d3ca7fba
JosueCarlosCamilo <camilo12lopezro@gmail.com>
2025-07-22 18:47:22 -0500

    main.py

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ WIDTH, HEIGHT = 800, 600
 ventana = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
 pygame.display.set_caption("Simulador de vida salvaje")
 print("Nueva rama")
+#La rama esta en el branches
